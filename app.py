fa81224d2b7361e787a18e5dba3e8b318a0729bf
deepak-usa <datas.deepak7@gmail.com>
2026-06-10 21:36:11 +0530

    Remove unused ffmpeg import from app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 import cv2
 import numpy as np
-from flask import Flask, request, send_file, render_template_string
+from flask import Flask, request, send_file
 from flask_cors import CORS
-import ffmpeg
+
 
 app = Flask(__name__)
 CORS(app)
